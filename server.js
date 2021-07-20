17da7a718158ac6ab6f6fbdb8ae833d8e954029c
Brian Rodgers Vargo <brv@Brians-MacBook-Pro.local>
2021-07-20 19:33:34 -0400

    added test route

--- server.js
+++ server.js
@@ -26,6 +26,6 @@ app.listen(port, () => {
 
 
 // respond with "hello world" when a GET request is made to the homepage
-app.get('/', function (req, res) {
+app.get('/test', function (req, res) {
   res.send('hello world')
 })
